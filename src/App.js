9030a1f0ee083af26c443f6f7aec95370da70d54
mobugirit <justin1211@snu.ac.kr>
2023-06-13 09:29:12 +0900

    mm

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import Detail from "./routes/Detail"
 function App() {
   return <Router>
     <Routes>
-      <Route path="/hello" element={<h1>hello</h1>} />
-      <Route path="/movie/:id" element={<Detail />} />
-      <Route path="/" element={<Home />} />
+      <Route path={`${process.env.PUBLIC_URL}/hello`} element={<h1>hello</h1>} />
+      <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
     </Routes>
   </Router>;
 }
